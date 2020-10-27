96ca0fda3c0a01c9147267417046a615b40cffbc
lzx <15110124387@163.com>
2020-10-27 14:29:18 +0800

    默认更改列表

---
+++ src/main/java/com/lq/maintenance/security/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.lq.maintenance.security;
+
+import com.lq.maintenance.core.model.SysUser;
+import com.lq.maintenance.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+* @Title 用户登录认证信息查询
+* @Description
+* @author LZX
+* @date 2020/10/27 14:28
+*/
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        SysUser user = userService.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("该用户不存在");
+        }
+        // 用户权限列表，根据用户拥有的权限标识与如 @PreAuthorize("hasAuthority('sys:menu:view')") 标注的接口对比，决定是否可以调用接口
+        Set<String> permissions = userService.findPermissions(username);
+        List<GrantedAuthority> grantedAuthorities = permissions.stream().map(GrantedAuthorityImpl::new).collect(Collectors.toList());
+        return new JwtUserDetails(username, user.getUserPassword(), grantedAuthorities);
+    }
+}
